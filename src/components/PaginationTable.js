c367cdb8b7c43b60365078126c1aad0adc4f502b
DarioMC <dariomc_95@yahoo.es>
2021-03-04 14:31:51 -0800

    Pagination (page size)

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -20,6 +20,9 @@ export const PaginationTable = () => {
             canPreviousPage,
             prepareRow,
             pageOptions,
+            gotoPage,
+            pageCount,
+            setPageSize,
             state,
             setGlobalFilter,
         } = useTable({
@@ -28,7 +31,7 @@ export const PaginationTable = () => {
         },
         useGlobalFilter, useSortBy, usePagination)
 
-        const { globalFilter, pageIndex } = state
+        const { globalFilter, pageIndex, pageSize } = state
 
     return (
         <>
@@ -71,8 +74,29 @@ export const PaginationTable = () => {
                     {pageIndex + 1} of {pageOptions.length}
                 </strong>{' '}
             </span>
+            <span>
+                | Go to page: {' '}
+                <input  type='number' defaultValue={pageIndex +1} 
+                onChange={e => {
+                    const pageNumber = e.target.value ? Number(e.target.value) -1 : 0
+                    gotoPage(pageNumber)
+                }} 
+                style={{width: '50px'}} />
+            </span>
+            <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+                {
+                    [10, 25, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))
+                }
+
+            </select>
+            <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
             <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
             <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+            <button onClick={() => gotoPage(pageCount - 1)} disabled={!canPreviousPage}>{'>>'}</button>
         </div>
         </>
     )
